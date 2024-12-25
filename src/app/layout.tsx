6fe8685f511c172b51c0bb1731b7787a99513898
furkanqq <133240374+furkanqq@users.noreply.github.com>
2024-12-25 13:35:09 +0300

    ss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" suppressHydrationWarning>
       <head>
         <AdSense pId={'6764792523122888'} />
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        {/* <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" /> */}
       </head>
       <body className={inter.className}>
         <Providers>
